e5cacdd215737cd05656ec0c87f67de388854b05
Josh Cirre <joshcirre@gmail.com>
2024-01-08 17:03:50 -0700

    add update with message regarding class api

--- src/GenesisServiceProvider.php
+++ src/GenesisServiceProvider.php
@@ -13,14 +13,23 @@ class GenesisServiceProvider extends ServiceProvider
     public function boot()
     {
         UiCommand::macro('genesis', function ($command) {
-            // Check for the 'class' option within the passed options
-            $useClassFolder = collect($command->option('option'))->contains('class');
+            $useClassFolder = false;
+
+            // Retrieve all options as an array and check if 'class' is in the array
+            $options = $command->option('option');
+            if (is_array($options)) {
+                $useClassFolder = in_array('class', $options);
+            } else if ($options === 'class') {
+                // If it's not an array, check if it's a string 'class'
+                $useClassFolder = true;
+            }
 
             // Determine the stubs directory based on the presence of the 'class' option
             $stubDirectoryType = $useClassFolder ? 'class' : 'functional';
             GenesisPreset::install($stubDirectoryType);
 
-            $command->info('Genesis starter kit installed successfully.');
+            $message = $useClassFolder ? 'Genesis starter kit with Volt Class API installed successfully.' : 'Genesis starter kit installed successfully.';
+            $command->info($message);
         });
 
         // Registering package commands.
