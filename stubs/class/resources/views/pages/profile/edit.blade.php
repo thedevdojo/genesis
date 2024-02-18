57fbcbc0ab7fbd171f5f93464e4863cccf4dd62b
Tony Lea <tnylea@gmail.com>
2024-02-18 16:32:51 -0500

    Adding some updates to the layouts

--- stubs/class/resources/views/pages/profile/edit.blade.php
+++ stubs/class/resources/views/pages/profile/edit.blade.php
@@ -103,8 +103,8 @@ new class extends Component {
     </x-slot>
 
     @volt('profile.edit')
-        <div class="py-12">
-            <div class="mx-auto space-y-6 max-w-7xl sm:px-6 lg:px-8">
+        <div class="pb-5">
+            <div class="mx-auto space-y-6">
 
                 {{-- Update Profile Section --}}
                 <section
