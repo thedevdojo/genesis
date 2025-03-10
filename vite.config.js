9c66c9ff9bc33c07b356516373febc531d8e1aa2
Tony Lea <tony@devdojo.com>
2025-03-10 14:28:47 -0400

    Adding latest updates

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,18 @@
-import { defineConfig } from 'vite';
+import {
+    defineConfig
+} from 'vite';
 import laravel from 'laravel-vite-plugin';
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [`resources/views/**/*`],
         }),
         tailwindcss(),
     ],
+    server: {
+        cors: true,
+    },
 });
